0a35aef893412b93009a0468208713f47e9344cc
Mahikhalid123 <mehmoodkhalid.mtb@gmail.com>
2025-11-10 21:07:03 +0500

    Update Projects.jsx

--- src/components/project/Projects.jsx
+++ src/components/project/Projects.jsx
@@ -111,7 +111,7 @@ export default function Projects() {
           </span>
         </h2>
 
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-15">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-15 place-items:center">
           {projects.map((project, idx) => (
             <ProjectCard key={idx} project={project} />
           ))}
